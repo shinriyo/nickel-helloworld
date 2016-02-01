b632e3a7d3057ec1541be5c94c4245dae4af4906
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-02-01 09:02:50 +0900

    テンプレ置き換えやDB修正

--- src/main.rs
+++ src/main.rs
@@ -12,29 +12,43 @@ use postgres::{Connection, SslMode};
 use std::collections::HashMap;
 
 // モデル
-struct EmployeeInfo {
-    EmpNo: i32,
-    EmpName: String,
-    Salary: i32,
-    DeptName: String,
-    Designation: String,
+struct Movie {
+    id: i32,
+    title: String,
+    director: String,
+    releaseYear: i32,
+    genre: String,
 }
+//struct EmployeeInfo {
+//    EmpNo: i32,
+//    EmpName: String,
+//    Salary: i32,
+//    DeptName: String,
+//    Designation: String,
+//}
 
 // テーブルのセットアップ
 fn setup_table<'a>(req: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
     let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
-
     // also print to stdout
-    return match conn.execute("CREATE TABLE EmployeeInfo (
-        EmpNo       SERIAL PRIMARY KEY,
-        EmpName     VARCHAR (50) NOT NULL,
-        Salary      DECIMAL (18) NOT NULL,
-        DeptName    VARCHAR (50) NOT NULL,
-        Designation VARCHAR (50) NOT NULL
+    return match conn.execute("CREATE TABLE Movie (
+        id          SERIAL PRIMARY KEY,
+        title       VARCHAR (50) NOT NULL,
+        director    DECIMAL (18) NOT NULL,
+        releaseYear DECIMAL (4)  NOT NULL,
+        genre       VARCHAR (50) NOT NULL
     )",
+    // also print to stdout
+//    return match conn.execute("CREATE TABLE EmployeeInfo (
+//        EmpNo       SERIAL PRIMARY KEY,
+//        EmpName     VARCHAR (50) NOT NULL,
+//        Salary      DECIMAL (18) NOT NULL,
+//        DeptName    VARCHAR (50) NOT NULL,
+//        Designation VARCHAR (50) NOT NULL
+//    )",
     &[]) {
         // http://www.rust-ci.org/Indiv0/paste/doc/nickel/struct.Response.html
-        Ok(n) => return res.send("EmployeeInfo table was created."),
+        Ok(n) => return res.send("Movie table was created."),
         // エラー
         // Err(err) => return res.send(println!("Error running query: {:?}", err))
         Err(err) => return res.send(format!("Error running query: {:?}", err))
@@ -45,7 +59,7 @@ fn setup_table<'a>(req: &mut Request, res: Response<'a>) -> MiddlewareResult<'a>
 fn index<'a>(req: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
     let mut data = HashMap::new();
     data.insert("name", "user");
-    return res.render("app/employee/views/index.tpl", &data);
+    return res.render("app/movie/views/index.tpl", &data);
 }
 
 // 基本ページ
@@ -74,22 +88,36 @@ fn edit_employee<'a>(req: &mut Request, res: Response<'a>) -> MiddlewareResult<'
 fn show_employees<'a>(req: &mut Request, res: Response<'a>) -> MiddlewareResult<'a> {
     let conn = Connection::connect("postgres://postgres@localhost", SslMode::None).unwrap();
     let mut data = HashMap::new();
+    data.insert("name", "user");
     for row in &conn.query("SELECT EmpNo, EmpName, Salary, DeptName,
-        Designation FROM EmployeeInfo", &[]).unwrap() {
-        let employeeInfo = EmployeeInfo {
-            EmpNo: row.get(0),
-            EmpName: row.get(1),
-            Salary: row.get(2),
-            DeptName: row.get(3),
-            Designation: row.get(4)
-        };
+        Designation FROM Movie", &[]).unwrap() {
+//        let movie = Movie {
+//            id: row.get(0),
+//`director`, `genre`, `title`, `releaseYear`
+//        };
 
-        data.insert("EmpNo", employeeInfo.EmpNo.to_string());
-        data.insert("EmpName", employeeInfo.EmpName.to_string());
-        data.insert("Salary", employeeInfo.Salary.to_string());
-        data.insert("DeptName", employeeInfo.DeptName.to_string());
-        data.insert("Designation", employeeInfo.Designation.to_string());
+//        data.insert("EmpNo", employeeInfo.EmpNo.to_string());
+//        data.insert("EmpName", employeeInfo.EmpName.to_string());
+//        data.insert("Salary", employeeInfo.Salary.to_string());
+//        data.insert("DeptName", employeeInfo.DeptName.to_string());
+//        data.insert("Designation", employeeInfo.Designation.to_string());
     }
+//    for row in &conn.query("SELECT EmpNo, EmpName, Salary, DeptName,
+//        Designation FROM EmployeeInfo", &[]).unwrap() {
+//        let employeeInfo = EmployeeInfo {
+//            EmpNo: row.get(0),
+//            EmpName: row.get(1),
+//            Salary: row.get(2),
+//            DeptName: row.get(3),
+//            Designation: row.get(4)
+//        };
+//
+//        data.insert("EmpNo", employeeInfo.EmpNo.to_string());
+//        data.insert("EmpName", employeeInfo.EmpName.to_string());
+//        data.insert("Salary", employeeInfo.Salary.to_string());
+//        data.insert("DeptName", employeeInfo.DeptName.to_string());
+//        data.insert("Designation", employeeInfo.Designation.to_string());
+//    }
     return res.render("app/employee/views/show_employees.tpl", &data);
 }
 
@@ -108,21 +136,17 @@ fn main() {
     // URLのセット
     let mut router = Nickel::router();
 
-    // 普通のページ
-    router.get("/EmployeeInfoAPI/showemployees", show_employees);
-    router.get("/EmployeeInfoAPI/addemployee", add_employee);
-    router.get("/EmployeeInfoAPI/delete/", delete_employee);
-    router.get("/EmployeeInfoAPI/edit/", edit_employee);
-    router.get("/EmployeeInfoAPI/", index);
-
     // テーブル準備
-//    router.get("/setup/entries", setup_table);
+    router.get("/setup/movies", setup_table);
+    // 普通のページ
+    router.get("/", index);
+
     // API
-//    router.get("/api/entries", xx);
-//    router.post("/api/entries", xx);
-//    router.get("/api/entries/id", xx);
-//    router.put("/api/entries/id", xx);
-//    router.delete("/api/entries/id", xx);
+//    router.get("/api/movies", xx);
+//    router.post("/api/movies", xx);
+//    router.get("/api/movies/id", xx);
+//    router.put("/api/movies/id", xx);
+//    router.delete("/api/movies/id", xx);
 
     server.utilize(router);
     server.listen("localhost:6767");
